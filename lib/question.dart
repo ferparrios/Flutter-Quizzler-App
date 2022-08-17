ccb82d3ca9927f576aaadc41341ae3d3b2fd5fc1
Fernando Paredes Rios <fprios112@gmail.com>
2022-08-17 16:27:09 -0500

    finished the quizzler app

---
+++ lib/question.dart
@@ -0,0 +1,14 @@
+class Question {
+  
+  String questionText;
+  bool questionAnswer;
+
+  // Question({String q, bool a}){
+  //   questionText = q;
+  //   questionAnswer = a;
+  // }
+  Question(this.questionText, this.questionAnswer);
+
+}
+
+// Question newQuestion = Question(q: "text" , a: true);
